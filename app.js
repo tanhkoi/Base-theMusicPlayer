551796b99647e3ec10646c755a66d72437b1079e
tanhkoi <tankhoi46@gmail.com>
2023-03-14 21:48:46 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * 1. render song
+ * 2. srcoll top
+ * 3. play / pause / seek
+ * 4. CD rotate
+ * 5. next / previous
+ * 6. random
+ * 7. next / repeat when ended
+ * 8. active song
+ * 9. srcoll active song into view
+ * 10. play song when click
+ */
